19a23bf059938bbe331b8e0bdeaedb8bcd7be7ec
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2023-10-03 12:39:55 +0530

    added my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> My Name is Vineeth Kumar
+
         </p>
         <a
           className="App-link"
